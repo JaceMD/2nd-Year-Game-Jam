75ba0b2e5700c9ea850eaa128930245e83eb5198
Magnuswolf3 <43730403+Magnuswolf3@users.noreply.github.com>
2018-10-02 19:44:00 +0200

    Base Game
    Basic Game:
    Player can move, can search but no items, can press q to hide but can't actually hide
    Nun navMesh working, moves to players last location when the player searches.

---
+++ Assets/Scripts/NunScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NunScript : MonoBehaviour {
+
+	[SerializeField]
+
+	NavMeshAgent navAgent;
+
+	Vector3 destination;
+
+	bool suspects;							//Set to true if heard a noise and hasn't found anything
+	bool heardNoise;						//Set to true if initial noise is heard
+
+	// Use this for initialization
+	void Start () {
+		navAgent = this.GetComponent<NavMeshAgent> (); 
+		heardNoise = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (heardNoise) {
+			investigateNoise (destination);
+		}
+
+		if (!heardNoise && suspects) {
+			lookAtBeds ();
+		}
+
+		if (!heardNoise && !suspects) {
+			followGeneralPath ();
+		}
+	}
+
+	void followGeneralPath()
+	{
+		
+		
+	}
+		
+	void lookAtBeds()
+	{
+		GameObject[] bedList = GameObject.FindGameObjectsWithTag ("Bed");
+	}
+
+	public void investigateNoise(Vector3 noiseSource)
+	{
+		destination = noiseSource;
+
+
+		navAgent.SetDestination (destination);
+		heardNoise = true;
+
+		if (Vector3.Distance (transform.position, noiseSource) < 5.6f) {
+			heardNoise = false;
+			print ("Looked at");
+		}
+
+	}
+
+
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour {
+
+
+	public Text search;
+	public Text hide;
+	GameObject closestBed;
+	public float speed;
+	bool hiding;
+	bool searching;
+	bool delay;
+	public GameObject nunAI;
+
+	// Use this for initialization
+	void Start () {
+		search.enabled = false;
+		hide.enabled = false;
+		delay = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		playerMovement ();
+
+		closestBed = findClosestBed ();
+
+		if (closestBed != null) {
+			search.enabled = true;
+			hide.enabled = true;							//enables UI text when the player is close to a bed
+			playerControl ();
+
+		} else {
+			search.enabled = false;
+			hide.enabled = false;
+		}
+
+		if (hiding) {
+			print ("Hiding");
+		}
+
+		if (searching) {
+			print ("Searching");
+		}
+
+	}
+
+	void playerMovement()									//function that allows the player to move based on the speed input in the inspector
+	{
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.position = new Vector3 (transform.position.x - speed, transform.position.y, transform.position.z);
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.position = new Vector3 (transform.position.x + speed, transform.position.y, transform.position.z);
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z - speed);
+		}
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z + speed);
+		}
+	}
+
+	void playerControl()																	//function that allows the player to hide under beds and to search for stuff
+	{
+		if (Input.GetKey (KeyCode.E) && !hiding) {											//Button pressed to let player search.
+			searching = true;
+			nunAI.GetComponent<NunScript> ().investigateNoise (transform.position);
+		} else {
+			searching = false;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Q) && !searching && !hiding && !delay) {				//Button pressed to let player hide
+			hiding = true;
+			delay = true;
+			StartCoroutine (WaitABit());
+
+		}
+
+		if (Input.GetKeyDown (KeyCode.Q) && !searching && hiding && !delay) {
+			hiding = false;
+			delay = true;
+			StartCoroutine (WaitABit());
+		}
+	}
+
+
+	GameObject findClosestBed() 						//function that finds the closest bed and returns that object to the update
+	{
+		GameObject cBed = null;
+		GameObject[] bedList = GameObject.FindGameObjectsWithTag ("Bed");
+
+		for (int loop = 0; loop < bedList.Length; loop++) {
+			if (cBed == null) {
+				cBed = bedList [loop];
+			}
+
+			if (Vector3.Distance(transform.position, bedList[loop].transform.position) <= Vector3.Distance(transform.position, cBed.transform.position)) {
+				cBed = bedList[loop];
+			}
+		}
+
+		if (cBed != null) {														//returns null if the bed is out of the range of player
+			if (Mathf.Abs (transform.position.x - cBed.transform.position.x) > 4.8f || Mathf.Abs (transform.position.z - cBed.transform.position.z) > 5.7f) {
+				cBed = null;
+			}
+		}
+
+		return cBed;
+	}
+		
+	IEnumerator WaitABit()
+	{
+		yield return new WaitForSeconds (0.3f);
+		delay = false;
+	}
+}
