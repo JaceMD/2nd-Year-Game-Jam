c498c315f6957073902487812117bac0e314ad52
Magnuswolf3 <43730403+Magnuswolf3@users.noreply.github.com>
2018-10-03 10:40:37 +0200

    Iteration_2
    Item_Placement complete:
    There is now a bed manager that assigns items to certain beds and also assigns the player bed.
    It also makes sure that none of these repeat, so none of these will be placed in the same beds or placed on the player bed.

---
+++ Assets/Scripts/BedManagerScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BedManagerScript : MonoBehaviour {
+
+	int itemPlacement = 0;
+	int itemPlaced_1 = 0;
+	int itemPlaced_2 = 0;
+	int itemPlaced_3 = 0;
+	int itemPlaced_4 = 0;
+	int playerBed = 0;
+
+	// Use this for initialization
+	void Start () {
+
+		itemPlacement = Random.Range (1, 17);
+		playerBed = itemPlacement;
+
+		do{
+			itemPlacement = Random.Range(1,17);
+		}while(itemPlacement == playerBed);
+			
+		itemPlaced_1 = itemPlacement;
+
+		do{
+			itemPlacement = Random.Range(1,17);
+		}while(itemPlacement == playerBed||itemPlacement == itemPlaced_1);
+
+		itemPlaced_2 = itemPlacement;
+
+		do{
+			itemPlacement = Random.Range(1,17);
+		}while(itemPlacement == playerBed||itemPlacement == itemPlaced_1 || itemPlacement == itemPlaced_2);
+
+		itemPlaced_3 = itemPlacement;
+
+		do{
+			itemPlacement = Random.Range(1,17);
+		}while(itemPlacement == playerBed||itemPlacement == itemPlaced_1 || itemPlacement == itemPlaced_2 || itemPlacement == itemPlaced_3);
+
+		itemPlaced_4 = itemPlacement;
+
+
+		print ("Player bed at "+ playerBed);
+		print ("Item 1 at "+itemPlaced_1);
+		print ("Item 2 at "+itemPlaced_2);
+		print ("Item 3 at "+ itemPlaced_3);
+		print ("Item 4 at "+itemPlaced_4);
+
+
+		GameObject.Find ("Bed_" + playerBed).GetComponent<BedScript>().setPlayerBed();
+		GameObject.Find ("Bed_" + itemPlaced_1).GetComponent<BedScript>().setItem_1();
+		GameObject.Find ("Bed_" + itemPlaced_2).GetComponent<BedScript>().setItem_2();
+		GameObject.Find ("Bed_" + itemPlaced_3).GetComponent<BedScript>().setItem_3();
+		GameObject.Find ("Bed_" + itemPlaced_4).GetComponent<BedScript>().setItem_4();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/BedScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BedScript : MonoBehaviour {
+
+	bool item_1;
+	bool item_2;
+	bool item_3;
+	bool item_4;
+	bool playerBed;
+
+
+	// Use this for initialization
+	void Start () {
+		item_1 = false;
+		item_2 = false;
+		item_3 = false;
+		item_4 = false;
+		playerBed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void setItem_1()
+	{
+		item_1 = true;
+		print ("Item 1 placed here:" + this.gameObject.name);
+	}
+
+	public void setItem_2()
+	{
+		item_2 = true;
+		print ("Item 2 placed here:" + this.gameObject.name);
+	}
+
+	public void setItem_3()
+	{
+		item_3 = true;
+		print ("Item 3 placed here:" + this.gameObject.name);
+	}
+
+	public void setItem_4()
+	{
+		item_4 = true;
+		print ("Item 4 placed here:" + this.gameObject.name);
+	}
+
+	public bool anyItems()
+	{
+		if (item_1 || item_2 || item_3 || item_4) {
+			return true;
+		}
+		return false;
+	}
+
+	public void setPlayerBed()
+	{
+		playerBed = true;
+		print ("PlayerBed placed here:" + this.gameObject.name);
+	}
+
+}
